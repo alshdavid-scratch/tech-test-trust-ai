8835886f621b47c6789c181acc0b6933f2326637
David Alsh <alshdavid@gmail.com>
2024-09-12 17:26:34 +1000

    Removing warnings

--- client/src/cmd/views/home/home.tsx
+++ client/src/cmd/views/home/home.tsx
@@ -2,21 +2,19 @@ import './home.scss'
 import { h } from "preact";
 import { useInject } from '../../contexts/app.tsx';
 import { CategoryService } from '../../../platform/categories/category-service.ts';
-import { IntentsService } from '../../../platform/intents/intents-service.ts';
-import { useState } from 'preact/hooks';
-import { PieChart } from '../../components/pie-chart/pie-chart.tsx';
-import { classNames } from '../../../platform/elements/class-names.ts';
-import { Panel, PanelHeader, PanelList, PanelListItem, PanelSection } from '../../components/panel/panel.tsx';
 import { BarChart } from '../../components/bar-chart/bar-chart.tsx';
+import { PanelHeader } from '../../components/panel/panel.tsx';
 
 export function HomeView() {
   const categoriesService = useInject(CategoryService)
-  const intentsService = useInject(IntentsService)
-  const [selectedCategory, setSelectedCategory] = useState<string>('foo')
 
   const categoryIntents = categoriesService.getCategorySummary()
 
   return <div class="view-home">
+    <PanelHeader>
+      <div>Overview</div>
+    </PanelHeader>
+
     <BarChart 
       title="Number of hits per category" 
       xLabel="Hits" 
